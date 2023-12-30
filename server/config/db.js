df319ed5bf6161a3004dfbb2e946444e0bf01ed2
sachinmadhwa <kjhasachin@gmail.com>
2023-12-30 13:24:32 +0545

    login and register api done

--- server/config/db.js
+++ server/config/db.js
@@ -5,10 +5,11 @@ const connectDB = async () => {
   try {
     await mongoose.connect(process.env.MONGO_URL);
     console.log(
-      `connected to database ${mongoose.connection.host}`.bgCyan.white
+      `Connected To DATABASE ${mongoose.connection.host}`.bgCyan.white
     );
   } catch (error) {
     console.log(`error in connection DB ${error}`.bgRed.white);
   }
 };
+
 module.exports = connectDB;
--- server/server.js
+++ server/server.js
@@ -1,36 +1,31 @@
 const express = require("express");
-const colors = require("colors");
 const cors = require("cors");
 const dotenv = require("dotenv");
+const colors = require("colors");
 const morgan = require("morgan");
 const connectDB = require("./config/db");
 
 //DOTENV
 dotenv.config();
 
-// MONGODB CONNECTION 
+// MONGODB CONNECTION
 connectDB();
 
 //REST OBJECT
 const app = express();
 
-//MIDDLEWARES
+//middlewares
 app.use(cors());
 app.use(express.json());
 app.use(morgan("dev"));
 
 //ROUTES
-app.get("", (req, res) => {
-  res.status(200).json({
-    success: true,
-    message: "Welcome to full stack app",
-  });
-});
+app.use("/api/v1/auth", require("./routes/userRoutes"));
 
 //PORT
 const PORT = process.env.PORT || 8080;
 
-// LISTEN 
-app.listen(PORT,()=>{
-    console.log(`server running ${PORT}`.bgGreen.white)
+//listen
+app.listen(PORT, () => {
+  console.log(`Server Runnning ${PORT}`.bgGreen.white);
 });
